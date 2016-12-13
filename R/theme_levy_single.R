da969e15f87f628a0c032ca19bc5952b80edd0fb
Michael Levy <malevy@ucdavis.edu>
2016-12-13 12:34:14 -0800

    Added single panel theme without top and right edges

--- R/theme_levy_single.R
+++ R/theme_levy_single.R
@@ -1,4 +1,4 @@
-#' theme_levy
+#' theme_levy_single
 #'
 #' @param base_size 12 by default
 #' @param font "Source Sans Pro" by default. \code{extrafont::fonttable()[["FamilyName"]]} to see what's available
@@ -8,20 +8,17 @@
 #' @import ggplot2
 #' @importFrom extrafont fonttable
 #'
-#' @details 8 x 8" png should be about right for single-panel plot
-#'
-theme_levy <- function(base_size = 12, font = "Source Sans Pro") {
+theme_levy_single <- function(base_size = 12, font = "Source Sans Pro") {
 
   if(!font %in% extrafont::fonttable()[["FamilyName"]]) {
     warning("Font: \"", font, "\" not found. Using default sans instead.")
     font = "sans"
   }
 
-  theme_bw(base_size = base_size, base_family = font) %+replace%
+  theme_levy(base_size = base_size, font = font) %+replace%
     theme(
-      panel.grid = element_blank(),
-      strip.background = element_blank(),
-      strip.text = element_text(size = base_size, face = "bold")
+      axis.line = element_line(size = .2),
+      panel.border = element_blank()
     )
 }
 
